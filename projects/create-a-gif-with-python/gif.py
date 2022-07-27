530d33b52c80ae424ff6c275b681550a93ca3ca0
Sonny Li <sonnynomnom@gmail.com>
2022-07-26 20:48:05 -0400

    Update gif.py

--- projects/create-a-gif-with-python/gif.py
+++ projects/create-a-gif-with-python/gif.py
@@ -9,4 +9,4 @@ images = [ ]
 for filename in filenames:
   images.append(imageio.imread(filename))
 
-imageio.mimsave('movie.gif', images, 'GIF', duration = 1)
+imageio.mimsave('team.gif', images, 'GIF', duration = 1)
