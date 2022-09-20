86a6fc905df4e680aa71055dc7dc5a22ceb7c9e0
Sonny Li <sonnynomnom@gmail.com>
2022-09-20 17:01:56 -0400

    Update qr_code.py

--- projects/generate-a-qr-code-with-python/qr_code.py
+++ projects/generate-a-qr-code-with-python/qr_code.py
@@ -1,9 +1,10 @@
 import qrcode
 
-link_to_qrcode = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
 qr = qrcode.QRCode(version=1, box_size=5, border=5)
-qr.add_data(link_to_qrcode)
+qr.add_data(website_link)
 qr.make()
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("youtube_qr.png")
+img = qr.make_image(fill_color='black', back_color='white')
+img.save('youtube_qr.png')
