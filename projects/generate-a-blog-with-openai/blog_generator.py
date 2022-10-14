d511050b5b577ee64bafd0d60771d5c0ea872c5b
Sonny Li <sonnynomnom@gmail.com>
2022-10-14 10:24:48 -0400

    Create blog_generator.py

---
+++ projects/generate-a-blog-with-openai/blog_generator.py
@@ -0,0 +1,28 @@
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+
+openai.api_key = config['API_KEY']
+
+def generate_blog(paragraph_topic):
+  response = openai.Completion.create(
+    model = 'text-davinci-002',
+    prompt = 'Write a paragraph about the following topic.' + ' ' + paragraph_topic,
+    max_tokens = 400,
+    temperature = 0.3
+  )
+
+  retrieve_blog = response.choices[0].text
+
+  return retrieve_blog
+
+keep_writing = True
+
+while keep_writing:
+  answer = input('Write a paragraph? Y for yes, anything else for no')
+  if (answer == 'Y'):
+    paragraph_topic = input('What should this paragraph talk about?')
+    print(generate_blog(paragraph_topic))
+  else:
+    keep_writing = False
