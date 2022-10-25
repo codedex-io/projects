043e6ab6f8fed01c7d4b090cbfb7381c23acf8f9
Sonny Li <sonnynomnom@gmail.com>
2022-10-25 01:25:37 -0400

    Update scraper.py

--- projects/web-scrape-amazon-with-beautiful-soup/scraper.py
+++ projects/web-scrape-amazon-with-beautiful-soup/scraper.py
@@ -1,39 +1,38 @@
+# Web Scrape Amazon with Beautiful Soup 📦
+
 import requests
 from bs4 import BeautifulSoup
 
-
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.5"
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+  "Accept-Language": "en-US,en;q=0.5"
 }
 
 
 def get_product_details(product_url: str) -> dict:
+  # Create an empty product details dictionary
+  product_details = {}
+  
+  # Get the product page content and create a soup
+  page = requests.get(product_url, headers=headers)
+  soup = BeautifulSoup(page.content, features="lxml")
+  try:
+    # Scrape the product details
+    title = soup.find(
+      "span", attrs={"id": "productTitle"}).get_text().strip()
+    extracted_price = soup.find(
+      "span", attrs={"class": "apexPriceToPay"}).get_text().strip()
+    price = "$" + extracted_price.split("$")[1]
 
-    # Create an empty product details dictionary
-    product_details = {}
-
-    # Get the product page content and create a soup
-    page = requests.get(product_url, headers=headers)
-    soup = BeautifulSoup(page.content, features="lxml")
-    try:
-        # Scrape the product details
-        title = soup.find(
-            "span", attrs={"id": "productTitle"}).get_text().strip()
-        extracted_price = soup.find(
-            "span", attrs={"class": "apexPriceToPay"}).get_text().strip()
-        price = "$" + extracted_price.split("$")[1]
-
-        # Adding it to the product details dictionary
-        product_details["title"] = title
-        product_details["price"] = price
-
-        # Return the product details dictionary
-        return product_details
-    except Exception as e:
-        print("Could not fetch product details")
-        print(f"Failed with exception: {e}")
-
+    # Adding it to the product details dictionary
+    product_details["title"] = title
+    product_details["price"] = price
+    
+    # Return the product details dictionary
+    return product_details
+  except Exception as e:
+    print("Could not fetch product details")
+    print(f"Failed with exception: {e}")
 
 product_url = input("Enter product url: ")
 product_details = get_product_details(product_url)
