4afc5e8418d9e076277931a7fc0c2d17f5d8c106
Dusch4593 <brandondusch@gmail.com>
2023-03-18 12:43:22 -0400

    implement some feedback

--- projects/build-a-pomodoro-app-with-html-css-js/app.js
+++ projects/build-a-pomodoro-app-with-html-css-js/app.js
@@ -1,7 +1,7 @@
 const bells = new Audio('./sounds/bell.wav'); 
 const startBtn = document.querySelector('.btn-start'); 
 const session = document.querySelector('.minutes');
-console.log(session)
+
 let myInterval; 
 let state = true;
 
