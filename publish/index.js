4f3d3e0ae0a7e1ab4b824e4ccd10b3ae1fda9ce4
Goku-kun <jethvadharmarajsinh@gmail.com>
2023-04-12 00:03:38 +0530

    Add: Firebase publish GitHub action

---
+++ publish/index.js
@@ -0,0 +1,121 @@
+import firebaseAdmin from "firebase-admin";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import remarkPresetLintConsistent from "remark-preset-lint-consistent";
+import remarkPresetLintRecommended from "remark-preset-lint-recommended";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkBreaks from "remark-breaks";
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert({
+    type: "service_account",
+    project_id: "codedex-io",
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.client_cert_url,
+  }),
+});
+
+const firestore = firebaseAdmin.firestore();
+
+function getProjectFolderNames() {
+  return fs.readdirSync(path.resolve(process.cwd(), "projects"));
+}
+
+function getProjectMdxFile(projectFolderName) {
+  const files = fs.readdirSync(
+    path.resolve(process.cwd(), "projects", projectFolderName)
+  );
+
+  for (let file of files) {
+    if (file.endsWith(".mdx")) {
+      return file;
+    }
+  }
+  return null;
+}
+
+main();
+
+async function main() {
+  const projectFolders = getProjectFolderNames();
+
+  for (let projectFolderName of projectFolders) {
+    const projectMdxFile = getProjectMdxFile(projectFolderName);
+    if (!projectMdxFile) {
+      continue;
+    }
+
+    const fileNameWithoutExtension = projectMdxFile.split(".")[0];
+    const projectExists = (
+      await firestore.collection("projects").doc(fileNameWithoutExtension).get()
+    ).exists;
+
+    const projectContent = fs.readFileSync(
+      path.resolve(
+        process.cwd(),
+        "projects",
+        projectFolderName,
+        projectMdxFile
+      ),
+      "utf-8"
+    );
+
+    if (!projectExists) {
+      const projectMatter = matter(projectContent);
+
+      const source = await serialize(projectMatter.content, {
+        mdxOptions: {
+          remarkPlugins: [
+            remarkPresetLintConsistent,
+            remarkPresetLintRecommended,
+            remarkBreaks,
+            remarkGfm,
+          ],
+          rehypePlugins: [rehypeSlug, rehypeHighlight, rehypeExternalLinks],
+        },
+      });
+
+      await firestore
+        .collection("projects")
+        .doc(fileNameWithoutExtension)
+        .set(
+          {
+            source,
+            content: projectMatter.content,
+            ...projectMatter.data,
+            dateCreated: projectMatter.data.dateCreated.toUTCString(),
+            dateUpdated: new Date().toUTCString(),
+            likes: 0,
+            link: fileNameWithoutExtension,
+          },
+          { merge: true }
+        );
+    } else {
+      await firestore
+        .collection("projects")
+        .doc(fileNameWithoutExtension)
+        .set(
+          {
+            source,
+            content: projectMatter.content,
+            ...blogMatter.data,
+            dateCreated: blogMatter.data.dateCreated.toUTCString(),
+            dateUpdated: new Date().toUTCString(),
+            link: fileNameWithoutExtension,
+          },
+          { merge: true }
+        );
+    }
+  }
+}
