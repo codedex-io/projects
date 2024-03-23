b12f5d66d00ce9d7fbc53fdd29640e1d080e05a5
intelagense <18331781+intelagense@users.noreply.github.com>
2024-03-23 07:49:37 -0500

    Formats `if` statements to align with Learning Environment examples

--- projects/view-weather-with-html-css-js/weather-app/script.js
+++ projects/view-weather-with-html-css-js/weather-app/script.js
@@ -7,7 +7,7 @@ async function fetchWeather() {
   const apiKey = "REPLACE WITH YOUR API KEY"
 
 
-  if(searchInput == "") {
+  if (searchInput == "") {
     weatherDataSection.innerHTML = `
     <div>
     <h2>Empty Input!</h2>
@@ -23,13 +23,13 @@ async function fetchWeather() {
     const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`
 
     const response = await fetch(geocodeURL);
-    if(!response.ok) {
+    if (!response.ok) {
       console.log("Bad response! ", response.status);
       return;
     }
 
     const data = await response.json();
-    if(data.length == 0) {
+    if (data.length == 0) {
       console.log("Something went wrong here.");
       weatherDataSection.innerHTML = `
       <div>
