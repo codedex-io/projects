f9545d4d2f77960810e1b5dd91b58f107d860dda
copycode69 <jaytiwari092@gmail.com>
2024-10-13 14:49:42 +0530

    Ball Position Reset: The ball did not reset correctly after going out of bounds. The ball_rect position is now set to the center of the screen when it goes out of bounds
    Collision Handling: The code had issues with the ball not bouncing off the paddles correctly due to the ball's position not being adjusted after a collision. Adjustments were made to ensure the ball is moved outside the paddle after a collision.
    
    Boundary Checks: The original code had incorrect boundary checks for the ball. The code now checks for the top and bottom boundaries properly.

--- projects/build-pong-with-pygame/main.py
+++ projects/build-pong-with-pygame/main.py
@@ -1,203 +1,159 @@
 import pygame
 import random
 
-# constants for the windows width and height values
+# Constants for the window's width and height values
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 720
 
-# the RGB values for the colors used in the game
+# The RGB values for the colors used in the game
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
-def main(): 
-  # GAME SETUP
-  
-  # initialize the PyGame library (this is absolutely necessary)
-  pygame.init()
+# Constants for paddle speed and ball speed
+PADDLE_SPEED = 5
+BALL_SPEED = 4
 
-  # this creates the window for the game
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
-  # set the window's title
-  pygame.display.set_caption('Pong')
+def main():
+    # GAME SETUP
 
-  # create the clock object to keep track of the time
-  clock = pygame.time.Clock()
-  
-  '''
-  this is to check wheter or not to move the ball
-  we will make it move after 3 seconds
-  '''
-  started = False
-  
-  '''
-  these are the players' game paddles
-  the `Rect` function need the x, y, width and height
-  of the rectangles we will be drawing
-  '''
-  paddle_1_rect = pygame.Rect(30, 0, 7, 100)
-  paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
+    # Initialize the Pygame library (this is absolutely necessary)
+    pygame.init()
 
-  # this is to track by how much the players' paddles will move per frame
-  paddle_1_move = 0
-  paddle_2_move = 0
+    # This creates the window for the game
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-  # this is the rectangle that represents the ball
-  ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+    # Set the window's title
+    pygame.display.set_caption('Pong')
 
-  # determine the x and y speed of the ball 
-  ball_accel_x = random.randint(2, 4) * 0.1
-  ball_accel_y = random.randint(2, 4) * 0.1
+    # Create the clock object to keep track of the time
+    clock = pygame.time.Clock()
 
-  # randomize the direction of the ball
-  if random.randint(1, 2) == 1:
-    ball_accel_x *= -1
-  if random.randint(1, 2) == 1:
-    ball_accel_y *= -1
+    # This is to check whether or not to move the ball
+    started = False
 
-  # GAME LOOP
-  while True:
-    
-    ''' 
-    set the back ground color to black
-    needs to be called everytime the game updates
-    '''
-    screen.fill(COLOR_BLACK)
-    
-    # make the ball move after 3 seconds
-    if not started:
-      # load the Consolas font
-      font = pygame.font.SysFont('Consolas', 30)
-      
-      # draw some text to the center of the screen
-      text = font.render('Press Space to Start', True, COLOR_WHITE)
-      text_rect = text.get_rect()
-      text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-      screen.blit(text, text_rect)
-      
-      # update the display
-      pygame.display.flip()
-      clock.tick(60)
+    # These are the players' game paddles
+    paddle_1_rect = pygame.Rect(30, 0, 7, 100)
+    paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
 
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          pygame.quit()
-          return
-        if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_SPACE:
-            started = True
+    # This is to track by how much the players' paddles will move per frame
+    paddle_1_move = 0
+    paddle_2_move = 0
 
-       continue
+    # This is the rectangle that represents the ball
+    ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
 
-    '''
-    get the time elapse between now and the last frame
-    60 is an arbitrary number but the game runs smooth at 60 FPS
-    '''
-    delta_time = clock.tick(60)
+    # Determine the x and y speed of the ball 
+    ball_accel_x = random.choice([-BALL_SPEED, BALL_SPEED])
+    ball_accel_y = random.randint(2, 4) * 0.1
 
-    # checking for events
-    for event in pygame.event.get():
+    # GAME LOOP
+    while True:
 
-      # if the user exits the window
-      if event.type == pygame.QUIT:
+        # Set the background color to black
+        screen.fill(COLOR_BLACK)
 
-        # exit the function, to finish the game
-        return
+        # Make the ball move after pressing space
+        if not started:
+            # Load the Consolas font
+            font = pygame.font.SysFont('Consolas', 30)
 
-      # if the user is pressing a key
-      if event.type == pygame.KEYDOWN:
+            # Draw some text to the center of the screen
+            text = font.render('Press Space to Start', True, COLOR_WHITE)
+            text_rect = text.get_rect()
+            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+            screen.blit(text, text_rect)
 
-        # PLAYER 1
-        # if the key is W, set the movement of paddle_1 to go up
-        if event.key == pygame.K_w:
-          paddle_1_move = -0.5
+            # Update the display
+            pygame.display.flip()
+            clock.tick(60)
 
-        # if the key is S, set the movement of paddle_1 to go down
-        if event.key == pygame.K_s:
-          paddle_1_move = 0.5
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        started = True
 
-        # PLAYER 2
-        # if the key is the up arrow, set the movement of paddle_2 to go up
-        if event.key == pygame.K_UP:
-          paddle_2_move = -0.5
-        # if the key is the down arrow, set the movement of paddle_2 to go down
-        if event.key == pygame.K_DOWN:
-          paddle_2_move = 0.5
+            continue
 
-      # if the player released a key
-      if event.type == pygame.KEYUP:
-        # if the key released is w or s, stop the movement of paddle_1
-        if event.key == pygame.K_w or event.key == pygame.K_s: 
-          paddle_1_move = 0.0
+        # Checking for events
+        for event in pygame.event.get():
+            # If the user exits the window
+            if event.type == pygame.QUIT:
+                return
 
-        # if the key released is the up or down arrow, stop the movement of paddle_2
-        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-          paddle_2_move = 0.0
+            # If the user is pressing a key
+            if event.type == pygame.KEYDOWN:
+                # PLAYER 1
+                if event.key == pygame.K_w:
+                    paddle_1_move = -PADDLE_SPEED
+                if event.key == pygame.K_s:
+                    paddle_1_move = PADDLE_SPEED
 
-    '''
-    move paddle_1 and paddle_2 according to their `move` variables
-    we also multiply the `move` variable by the delta time to keep movement consistent through frames
-    '''
-    paddle_1_rect.top += paddle_1_move * delta_time
-    paddle_2_rect.top += paddle_2_move * delta_time
+                # PLAYER 2
+                if event.key == pygame.K_UP:
+                    paddle_2_move = -PADDLE_SPEED
+                if event.key == pygame.K_DOWN:
+                    paddle_2_move = PADDLE_SPEED
 
-    # if paddle_1 is going out of the screen by the top, set it to the maximum to limit its movement
-    if paddle_1_rect.top < 0:
-      paddle_1_rect.top = 0
-    
-    # paddle_2 is going out of the screen by the bottom, do the same thing   
-    if paddle_1_rect.bottom > SCREEN_HEIGHT:
-      paddle_1_rect.bottom = SCREEN_HEIGHT
+            # If the player released a key
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_s:
+                    paddle_1_move = 0.0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    paddle_2_move = 0.0
 
-    # do the same thing with paddle_2
-    if paddle_2_rect.top < 0:
-      paddle_2_rect.top = 0
-    if paddle_2_rect.bottom > SCREEN_HEIGHT:
-      paddle_2_rect.bottom = SCREEN_HEIGHT      
+        # Move paddle_1 and paddle_2 according to their `move` variables
+        paddle_1_rect.top += paddle_1_move
+        paddle_2_rect.top += paddle_2_move
 
-    # if the ball is getting close to the top (15 is an arbitrary number, but I found that it worked great)
-    if ball_rect.top < 0:
-      # invert its vertical velocity 
-      ball_accel_y *= -1
-      # add a bit of y to it to not trigger the above code again
-      ball_rect.top = 0
-    # do the same thing with the bottom
-    if ball_rect.bottom > SCREEN_HEIGHT - ball_rect.height:
-      ball_accel_y *= -1
-      ball_rect.top = SCREEN_HEIGHT - ball_rect.heigh
+        # Keep paddles within screen bounds
+        if paddle_1_rect.top < 0:
+            paddle_1_rect.top = 0
+        if paddle_1_rect.bottom > SCREEN_HEIGHT:
+            paddle_1_rect.bottom = SCREEN_HEIGHT
+        if paddle_2_rect.top < 0:
+            paddle_2_rect.top = 0
+        if paddle_2_rect.bottom > SCREEN_HEIGHT:
+            paddle_2_rect.bottom = SCREEN_HEIGHT
 
-    # if the ball goes out of bounds, end the game 
-    if ball_rect.left <= 0 or ball_rect.left >= SCREEN_WIDTH:
-      return
+        # Ball boundary checks
+        if ball_rect.top < 0 or ball_rect.bottom > SCREEN_HEIGHT:
+            ball_accel_y *= -1  # Invert vertical velocity
+            if ball_rect.top < 0:
+                ball_rect.top = 0
+            if ball_rect.bottom > SCREEN_HEIGHT - ball_rect.height:
+                ball_rect.top = SCREEN_HEIGHT - ball_rect.height
 
-    '''
-    if paddle_1_rect collides with the ball and the ball is in front of it, 
-    change the speed of the ball and make it move a little in the other way
-    '''
-    if paddle_1_rect.colliderect(ball_rect) and paddle_1_rect.left < ball_rect.left:
-      ball_accel_x *= -1
-      ball_rect.left += 5
-    # do the same with paddle_2_rect
-    if paddle_2_rect.colliderect(ball_rect) and paddle_2_rect.left > ball_rect.left:
-      ball_accel_x *= -1
-      ball_rect.left -= 5
+        # If the ball goes out of bounds, reset its position
+        if ball_rect.left <= 0 or ball_rect.left >= SCREEN_WIDTH:
+            ball_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Reset ball position
+            ball_accel_x = random.choice([-BALL_SPEED, BALL_SPEED])  # Reset ball speed
+            ball_accel_y = random.randint(2, 4) * 0.1
 
-    # if the game is started (after 3 seconds this is true)
-    if started:
-      # move the ball
-      ball_rect.left += ball_accel_x * delta_time 
-      ball_rect.top += ball_accel_y * delta_time
+        # Ball collision with paddles
+        if paddle_1_rect.colliderect(ball_rect):
+            ball_accel_x *= -1
+            ball_rect.left = paddle_1_rect.right  # Move ball outside paddle
+        if paddle_2_rect.colliderect(ball_rect):
+            ball_accel_x *= -1
+            ball_rect.right = paddle_2_rect.left  # Move ball outside paddle
 
-    # draw player 1 and player 2's rects with the white color
-    pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
-    pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+        # Move the ball
+        if started:
+            ball_rect.left += ball_accel_x
+            ball_rect.top += ball_accel_y
 
-    # draw the ball with the white color
-    pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
+        # Draw player paddles and ball
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+        pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
 
-    # update the display (this is necessary for Pygame)
-    pygame.display.update()
+        # Update the display
+        pygame.display.update()
+        clock.tick(60)
 
-# run the game
+# Run the game
 if __name__ == '__main__':
-  main()
+    main()
