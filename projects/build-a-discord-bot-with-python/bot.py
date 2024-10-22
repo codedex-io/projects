89ad0696d02f783349f2ea2d6744e35e2f745cdc
copycode69 <jaytiwari092@gmail.com>
2024-10-22 22:09:24 +0530

    updated bot.py

--- projects/build-a-discord-bot-with-python/bot.py
+++ projects/build-a-discord-bot-with-python/bot.py
@@ -2,14 +2,15 @@ import discord
 import requests
 import json
 
-# update the url to https://meme-api.com/gimme
+# update the URL to https://meme-api.com/gimme
 def get_meme():
     response = requests.get('https://meme-api.com/gimme')
     json_data = json.loads(response.text)
     return json_data['url']
 
 class MyClient(discord.Client):
-    async def on_read(self):
+    # Correct the method name from 'on_read' to 'on_ready'
+    async def on_ready(self):
         print(f'Logged on as {self.user}')
 
     async def on_message(self, message):
